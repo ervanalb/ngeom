0c99431179a320fe0899ecd57ebf45ae8bc81b1a
Eric Van Albert <eric@van.al>
2024-09-23 21:41:26 -0400

    Write docs on the macro

--- ngeom/src/lib.rs
+++ ngeom/src/lib.rs
@@ -15,7 +15,7 @@
 //! and the option to implement the rest.
 //!
 //! ngeom ships with modules for rigid Euclidean [2D](re2) and [3D](re3) geometry.
-//! Modules for spaces with different dimensionality or metric can be written with the help of
+//! Spaces with different dimensionality or metric can be implemented with the help of
 //! auto-generated operations implementations from the [provided macro](ngeom_macros::geometric_algebra).
 //! With some effort, usage code can be written to be generic
 //! over the dimensionality or even the metric of the space.
@@ -24,6 +24,113 @@
 //! Understanding geometric algebra is helpful but optional,
 //! as the functions are named for their geometric interpretation.
 //! Lower-level functions named after their [geometric algebra expressions](algebraic_ops) are available if needed.
+
+/// Generate geometric operations on the given set of structs
+///
+/// This low-level macro can be used to generate different geometric algebras
+/// if the pre-made modules for [2D](re2) or [3D](re3) rigid Euclidean geometry
+/// are not sufficient.
+///
+/// Here are some reasons you may want to use this macro:
+/// * You want ownership of the geometry structs,
+///   e.g. so you can implement traits on them
+///   within the constraints of the [orphan rule](https://doc.rust-lang.org/book/ch10-02-traits.html)
+/// * You want to rename the structs or their fields
+/// * You want multivectors for different subsets of basis elements
+/// * You want a different metric, to implement hyperbolic space or spherical space
+/// * You want a different number of dimensions, e.g. 1D or 5D
+///
+/// Careful! This macro is a bit finnicky with regards to naming and ordering.
+/// See the notes in the following example.
+///
+/// ```
+/// use ngeom::geometric_algebra;
+///
+/// // These traits must be brought into scope before invoking the macro:
+/// use ngeom::scalar::*;
+/// use ngeom::ops::*;
+/// use ngeom::algebraic_ops::*;
+///
+/// // Define a geometric algebra with the given basis vectors, metric,
+/// // and structs representing multivectors.
+/// geometric_algebra! {
+///     // Use the basis![] pseudo-macro to call out the names of the basis vectors.
+///     // This also sets the sign of the anti-scalar,
+///     // which corresponds to the handedness of the space.
+///     basis![w, x, y];
+///
+///     // Basis vectors may be single letters,
+///     // or may be a common prefix plus a single character e.g. [e_0, e_1, e_2]
+///     // Higher-grade basis elements retain one copy of the prefix,
+///     // e.g. e_21, e_012
+///
+///     // Use the metric![] pseudo-macro to call out the metric of the space--
+///     // this is a list of numbers parallel to the basis,
+///     // where each number is the dot product of that basis vector with itself.
+///     metric![0, 1, 1];
+///
+///     #[multivector] // Use this pseudo-macro to call out that a struct is a multivector
+///     #[derive(Clone, Copy)] // Multivectors must be Copy for use in math expressions
+///     struct Vector<T> { // Multivectors must have a single generic parameter named T
+///         w: T, // each field must be type T (the scalar type)
+///         x: T,
+///         y: T,
+///         // Any basis components that are not present are assumed to be zero.
+///     }
+///
+///     #[multivector]
+///     #[derive(Clone, Copy)]
+///     struct Bivector<T> {
+///         wx: T, // the coefficient on the basis bivector w ∧ x
+///         wy: T, // the coefficient on the basis bivector w ∧ y
+///         xy: T, // the coefficient on the basis bivector x ∧ y
+///
+///         // The spelling of the field name sets the multiplication order,
+///         // which may introduce a negative sign.
+///         // (This won't fundamentally change the behavior of the algebra,
+///         // which is defined fully by the metric and sign of the anti-scalar.)
+///     }
+///
+///     #[multivector]
+///     #[derive(Clone, Copy)]
+///     struct AntiScalar<T> {
+///         wxy: T, // the coefficient on the basis trivector w ∧ x ∧ y
+///
+///         // The spelling of this field should match the order set by basis![].
+///         // Otherwise, anti-scalar operations like anti_mul() and anti_sqrt()
+///         // won't get automatically implemented.
+///     }
+///
+///     // The order the structs are defined is important--
+///     // the return type of a geometric operation will be the
+///     // *first struct* that can represent the output.
+///     // For example, if this `Multivector` struct were defined first,
+///     // all geometric operations would use it for their output type,
+///     // since it contains all of the basis components
+///     // and can therefore represent any multivector.
+///     // For this reason, more comprehensive structs should be defined last.
+///
+///     #[multivector]
+///     #[derive(Clone, Copy)]
+///     struct Multivector<T> {
+///         a: T, // A field whose name isn't composed of basis vectors
+///               // is assumed to be the scalar component
+///         w: T,
+///         x: T,
+///         y: T,
+///         wx: T,
+///         wy: T,
+///         xy: T,
+///         wxy: T,
+///     }
+/// }
+///
+/// // The geometric_algebra! macro will emit the contained code verbatim
+/// // (with the pseudo-macros stripped out.)
+/// // It will then append `impl` blocks for all applicable traits in
+/// // `ngeom::ops` and `ngeom::algebraic_ops`
+/// // across all of the given multivector structs.
+pub use ngeom_macros::geometric_algebra;
 
 /// Traits that govern the scalar data type used by ngeom
 ///
